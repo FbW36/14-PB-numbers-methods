8941a691457cb9e320a797bd9826b6b79c9703b2
Tomas <eliz.tomas23@gmail.com>
2020-10-15 14:04:24 +0200

    another solution added

--- tomas/index.js
+++ tomas/index.js
@@ -12,6 +12,13 @@ console.log('100em not a number -->', isNaN(valueToCheck))
 console.log('What is the type of NaN? -->', typeof NaN)
 
 // rounding
-
 let mult = .1 * .2
 console.log('rounded sum -->', mult.toFixed(2))
+
+// Use a method to make sure that only XY is printed
+let num1 = 12.26000000000033;
+console.log('only 12.26 is printed -->', num1.toFixed(2))
+let num2 = 0.63783990;
+console.log('only 0.6 is printed -->', num2.toFixed(1))
+let num3 = 1.842220;
+console.log('only 1.8422 is printed -->', num3.toFixed(4))
