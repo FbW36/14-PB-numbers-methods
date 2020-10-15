999c4bbf657f20adff2b8f8cd61b065312d29a6e
Tony Tanaka <tony.h.tanaka@gmail.com>
2020-10-15 13:34:32 +0200

    finished exercise

---
+++ tonytanaka/index.js
@@ -0,0 +1,16 @@
+// Question 1
+// NaN stands for Not-A-Number
+let ex1a = "hello" * 1;
+console.log(typeof ex1a);
+let ex1b = "100em";
+console.log(isNaN(ex1b));
+console.log(typeof NaN);
+// The type of NaN is number
+
+// Question 2
+console.log((0.1 * 0.2).toFixed(2));
+
+// Question 3
+console.log((12.26000000000033).toFixed(2));
+console.log((0.6378399).toFixed(1));
+console.log((1.84222).toFixed(4));
